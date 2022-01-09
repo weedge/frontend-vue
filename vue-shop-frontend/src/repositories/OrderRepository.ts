3a832c87d38e1c1c04e0bc9a074162d107700178
weedge <weege007@gmail.com>
2022-01-09 18:10:48 +0800

    add router, validation, store

--- vue-shop-frontend/src/repositories/OrderRepository.ts
+++ vue-shop-frontend/src/repositories/OrderRepository.ts
@@ -1,14 +1,15 @@
 import Http from "./clients/AxiosClient";
 const orderv1 = "/shop/api/v1";
+const resource= "/orders";
 
 export default {
   order(payload: any) {
-    return Http.post(`${orderv1}/order`, payload);
+    return Http.post(`${orderv1}${resource}`, payload);
   },
   getOrder(orderId: string | number) {
-    return Http.get(`${orderv1}/order/${orderId}`);
+    return Http.get(`${orderv1}${resource}/${orderId}`);
   },
   getUserOrders(uid: string | number) {
-    return Http.get(`${orderv1}/user/${uid}/orders`);
+    return Http.get(`${orderv1}/users/${uid}/${resource}`);
   },
 };
--- vue-shop-frontend/src/repositories/ShopItemRepository.ts
+++ vue-shop-frontend/src/repositories/ShopItemRepository.ts
@@ -1,14 +1,15 @@
 import Http from "./clients/AxiosClient";
 const shopv1 = "/shop/api/v1";
+const resource = "/item";
 
 export default {
   getItems() {
-    return Http.get(`${shopv1}/items`);
+    return Http.get(`${shopv1}${resource}`);
   },
   getItem(id: string | number) {
-    return Http.get(`${shopv1}/item/${id}`);
+    return Http.get(`${shopv1}${resource}/${id}`);
   },
   search(keyword: string) {
-    return Http.get(`${shopv1}/item/search?q=${keyword}`);
+    return Http.get(`${shopv1}/${resource}/search?q=${keyword}`);
   },
 };
